8bec0ac9d2d0066e3aed76f9d3700c229e65e959
lucastusch <lucas.tusch@gmail.com>
2023-01-30 09:12:06 +0100

    basis

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# pyqt6-color-chooser
+# Getting started with pyqt6-color-chooser
+This project uses [PyQt6](https://pypi.org/project/PyQt6/).
---
+++ src/main.js.py
@@ -0,0 +1,18 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QWidget, QGridLayout, QMessageBox, QLineEdit, QLabel, QPushButton
+
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+
+
+def main():
+    app = QApplication(sys.argv)
+    window = Window()
+    window.show()
+    sys.exit(app.exec())
+
+
+if __name__ == '__main__':
+    main()
