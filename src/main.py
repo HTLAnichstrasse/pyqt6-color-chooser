0f6a7cf697ed4047725ca7c5fd0f057fccdf0fdf
lucastusch <lucas.tusch@gmail.com>
2023-02-03 11:48:45 +0100

    window-ui func & correct-color-button func

--- src/main.py
+++ src/main.py
@@ -1,34 +1,54 @@
 import sys
 import random
-from PyQt6.QtWidgets import QApplication, QWidget, QGridLayout, QMessageBox, QLineEdit, QLabel, QPushButton, QMainWindow
+import math
+from PyQt6.QtWidgets import QApplication, QWidget, QGridLayout, QMessageBox, QLineEdit, QLabel, QPushButton, \
+    QMainWindow, QFileDialog, QVBoxLayout, QMenu, QMenuBar
 
 # constants
 FIELD_X_COUNT = 3
 FIELD_Y_COUNT = 3
-WINDOW_HEIGHT = 500
-WINDOW_WIDTH = 500
+WINDOW_HEIGHT = 400
+WINDOW_WIDTH = 800
 
 
-class Window(QWidget):
+# todo: Difficulties: Easy(2|2) Medium(3|3) Hard(4|4) Impossible(5|5)
+# todo: implement correct color-guess
+# todo: implement try's
+# todo: save game & load game
+
+
+class Button(QWidget):
     def __init__(self):
         super().__init__()
+
         self.rgb_correct = []
         self.rgb = []
 
         self.layout = QGridLayout()
-        self.layout.setContentsMargins(20, 20, 20, 20)
-        self.setFixedWidth(800)
-        self.setFixedHeight(700)
-        self.setLayout(self.layout)
 
-        self.main_window_ui()
+        self.window_ui()
         self.create_colors()
         self.add_button()
-        self.correct_color()
+        self.correct_color_button()
 
-    def main_window_ui(self):
+    def window_ui(self):
+        self.layout.setContentsMargins(20, 20, 20, 20)
+        self.setLayout(self.layout)
         self.setWindowTitle("Color Chooser")
         self.setFixedSize(WINDOW_WIDTH, WINDOW_HEIGHT)
+
+    def correct_color_button(self):
+        correct_color = self.correct_color()
+        button = QPushButton("Target-Color")
+
+        button.setStyleSheet(f"background-color:rgb({correct_color[0]},{correct_color[1]},{correct_color[2]})")
+        button.setFixedSize((400 - 50), (200 - 50))
+
+        font = button.font()
+        font.setPointSize(18)
+        button.setFont(font)
+
+        self.layout.addWidget(button, 0, len(self.rgb[0]) + 1)
 
     def correct_color(self):
         self.rgb_correct = self.rgb[random.randint(0, len(self.rgb) - 1)][random.randint(0, len(self.rgb[0]) - 1)]
@@ -40,8 +60,8 @@ class Window(QWidget):
                 button = QPushButton()
                 button.setStyleSheet(
                     f"background-color:rgb({self.rgb[x][y][0]},{self.rgb[x][y][1]},{self.rgb[x][y][2]})")
-                button.setFixedHeight(WINDOW_WIDTH // FIELD_X_COUNT)
-                button.setFixedHeight(WINDOW_HEIGHT // FIELD_Y_COUNT)
+                button.setFixedSize((WINDOW_WIDTH - 50) // (FIELD_X_COUNT * 2),
+                                    (WINDOW_HEIGHT - 50) // FIELD_Y_COUNT)
 
                 self.layout.addWidget(button, x, y)
 
@@ -72,9 +92,14 @@ class Window(QWidget):
 
 def main():
     app = QApplication(sys.argv)
-    Window().show()
 
-    sys.exit(app.exec())
+    window = Button()
+    window.show()
+
+    try:
+        app.exec()
+    except KeyboardInterrupt:
+        print("Quited Game, Interrupt")
 
 
 if __name__ == '__main__':
