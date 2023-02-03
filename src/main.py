03247e409f50b91a7d58844d0f74d6d0d490e6c3
lucastusch <lucas.tusch@gmail.com>
2023-02-03 17:08:36 +0100

    major rewrite
    changes:
    changed from multi-lists to one dict

--- src/main.py
+++ src/main.py
@@ -12,27 +12,29 @@ WINDOW_WIDTH = 800
 
 
 # todo: convert rgb & buttons from list to dict
+# lugas: eine color-Funktion, die für EINEN button EINE color erstellt => & dabei schaut, ob de color schon mal in an
+# anderen element vom dict ist
 # todo: Difficulties: Easy(2|2) Medium(3|3) Hard(4|4) Impossible(5|5)
 # todo: implement correct color-guess
 # todo: implement try's
 # todo: save game & load game
 
 
-class Button(QWidget):
+class Window(QWidget):
     def __init__(self):
         super().__init__()
-
-        self.rgb_correct = []
-        self.buttons = {}
-        self.rgb = []
-
         self.layout = QGridLayout()
+        self.buttons = {}
+        self.correct_color = []
 
+        self.run()
+
+    def run(self):
         self.window_ui()
-        self.create_colors()
-        self.add_button()
-        self.correct_color_button()
-        self.check_color_on_click()
+        self.create_guess_buttons()
+        self.get_correct_color()
+        self.create_correct_button()
+        self.check_clicked_button()
 
     def window_ui(self):
         self.layout.setContentsMargins(20, 20, 20, 20)
@@ -40,74 +42,70 @@ class Button(QWidget):
         self.setWindowTitle("Color Chooser")
         self.setFixedSize(WINDOW_WIDTH, WINDOW_HEIGHT)
 
-    def correct_color_button(self):
-        correct_color = self.correct_color()
-        button = QPushButton("Target-Color")
+    # --------------------------------------------------------------------------------
+    # --- guesser button ---
+    def check_for_color_duplicates(self, color):
+        for ele in self.buttons:
+            if self.buttons[ele] == color:
+                color = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
+                self.check_for_color_duplicates(color)
+                print("duplicate")
 
-        button.setStyleSheet(f"background-color:rgb({correct_color[0]},{correct_color[1]},{correct_color[2]})")
+    def create_color_for_button(self, button):
+        color = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
+        self.check_for_color_duplicates(color)
+        self.buttons[button] = color
+
+    def create_single_guess_button(self, x, y):
+        button = QPushButton()
+        self.create_color_for_button(button)
+
+        button.setStyleSheet(
+            f"background-color:rgb({self.buttons[button][0]}, {self.buttons[button][1]}, {self.buttons[button][2]})")
+        button.setFixedSize((WINDOW_WIDTH - 50) // (FIELD_X_COUNT * 2), (WINDOW_HEIGHT - 50) // FIELD_Y_COUNT)
+
+        self.layout.addWidget(button, x, y)
+
+    def create_guess_buttons(self):
+        # button_type = "guess_button"
+        for x in range(FIELD_X_COUNT):
+            for y in range(FIELD_Y_COUNT):
+                self.create_single_guess_button(x, y)
+
+    # --- check if clicked button is correct or incorrect ---
+    def get_correct_color(self):
+        self.correct_color = random.choice(list(self.buttons.values()))
+
+    def check_clicked_button(self):
+        # self.correct_color = random.choice(list(self.buttons.values()))
+        for ele in self.buttons:
+            if self.buttons[ele] == self.correct_color:
+                ele.clicked.connect(lambda: print("correct"))
+            else:
+                ele.clicked.connect(lambda: print("incorrect"))
+
+    # --- create correct button ---
+
+    def create_correct_button(self):
+        button = QPushButton("True Color")
+
+        button.setStyleSheet(
+            f"background-color:rgb({self.correct_color[0]}, {self.correct_color[1]}, {self.correct_color[2]})")
         button.setFixedSize((400 - 50), (200 - 50))
 
         font = button.font()
         font.setPointSize(18)
         button.setFont(font)
 
-        self.layout.addWidget(button, 0, len(self.rgb[0]) + 1)
+        self.layout.addWidget(button, 0, FIELD_X_COUNT + 1)
 
-    def correct_color(self):
-        self.rgb_correct = self.rgb[random.randint(0, len(self.rgb) - 1)][random.randint(0, len(self.rgb[0]) - 1)]
-        return self.rgb_correct
-
-    def check_color_on_click(self):
-        for element in self.buttons:
-            if self.buttons[element] == self.rgb_correct:
-                element.clicked.connect(lambda: self.correct_color_clicked())
-            else:
-                element.clicked.connect(lambda: print(f"incorrect"))
-
-    def correct_color_clicked(self):
-        print(f"correct")
-
-    def add_button(self):
-        for x in range(0, FIELD_Y_COUNT, 1):
-            for y in range(0, FIELD_X_COUNT, 1):
-                button = QPushButton()
-                button.setStyleSheet(
-                    f"background-color:rgb({self.rgb[x][y][0]},{self.rgb[x][y][1]},{self.rgb[x][y][2]})")
-                button.setFixedSize((WINDOW_WIDTH - 50) // (FIELD_X_COUNT * 2),
-                                    (WINDOW_HEIGHT - 50) // FIELD_Y_COUNT)
-
-                self.buttons[button] = self.rgb[x][y]
-                self.layout.addWidget(button, x, y)
-
-    def create_colors(self):
-        for x in range(0, FIELD_Y_COUNT, 1):
-            temp = []
-            for y in range(0, FIELD_X_COUNT, 1):
-                tmp = []
-                for three_colors in range(0, 3, 1):
-                    tmp.append(random.randint(0, 255))
-
-                duplicate = self.check_duplicate_in_array(tmp)
-                if duplicate:
-                    y -= 1
-                else:
-                    temp.append(tmp)
-
-            self.rgb.append(temp)
-
-    def check_duplicate_in_array(self, arr):
-        for element in self.rgb:
-            for e in element:
-                if arr != e:
-                    return 0
-                else:
-                    return 1
+    # --------------------------------------------------------------------------------
 
 
 def main():
     app = QApplication(sys.argv)
 
-    window = Button()
+    window = Window()
     window.show()
 
     try:
