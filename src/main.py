e4654cf96030ee01fcca3e8d98b0ad6f23bd22bb
lucastusch <lucas.tusch@gmail.com>
2023-02-06 14:46:15 +0100

    toolbar

--- src/main.py
+++ src/main.py
@@ -1,12 +1,18 @@
 import sys
 import random
+
+from PyQt6.QtGui import QIcon, QAction
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QApplication, QWidget, QGridLayout, QMessageBox, QLineEdit, QLabel, QPushButton, \
-    QMainWindow, QFileDialog, QVBoxLayout, QMenu, QMenuBar
+    QMainWindow, QFileDialog, QVBoxLayout, QMenu, QMenuBar, QToolBar, QStatusBar
 
 # constants
-FIELD_X_COUNT = 3
-FIELD_Y_COUNT = 3
+DIFFICULTIES = {
+    "easy": 2,
+    "medium": 3,
+    "hard": 4,
+    "impossible": 5
+}
 WINDOW_HEIGHT = 400
 WINDOW_WIDTH = 800
 
@@ -23,11 +29,13 @@ class Window(QWidget):
         self.buttons = {}
         self.correct_color = []
         self.tries = 0
+        self.active_difficulty = 2
         self.tries_label = None
         self.correct_color_button = None
 
     def run(self):
         self.window_ui()
+        self.navbar()
         self.create_guess_buttons()
         self.get_correct_color()
         self.create_correct_button()
@@ -42,7 +50,7 @@ class Window(QWidget):
 
     # --------------------------------------------------------------------------------
     # --- guesser button ---
-    def check_for_color_duplicates(self, color):
+    def check_for_color_duplicates(self, color: list[int, int, int]):
         for ele in self.buttons:
             if self.buttons[ele] == color:
                 color = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
@@ -53,19 +61,20 @@ class Window(QWidget):
         self.check_for_color_duplicates(color)
         self.buttons[button] = color
 
-    def create_single_guess_button(self, x, y):
+    def create_single_guess_button(self, x: int, y: int):
         button = QPushButton()
         self.create_color_for_button(button)
 
         button.setStyleSheet(
             f"background-color:rgb({self.buttons[button][0]}, {self.buttons[button][1]}, {self.buttons[button][2]})")
-        button.setFixedSize((WINDOW_WIDTH - 50) // (FIELD_X_COUNT * 2), (WINDOW_HEIGHT - 50) // FIELD_Y_COUNT)
+        button.setFixedSize((WINDOW_WIDTH - 50) // (self.active_difficulty * 2),
+                            (WINDOW_HEIGHT - 50) // self.active_difficulty)
 
         self.layout.addWidget(button, x, y)
 
     def create_guess_buttons(self):
-        for x in range(FIELD_X_COUNT):
-            for y in range(FIELD_Y_COUNT):
+        for x in range(0, self.active_difficulty):
+            for y in range(0, self.active_difficulty):
                 self.create_single_guess_button(x, y)
 
     # --- check if clicked button is correct or incorrect ---
@@ -80,7 +89,6 @@ class Window(QWidget):
                 ele.clicked.connect(lambda: self.update_tries_on_click("set"))
 
     # --- create correct button ---
-
     def create_correct_button(self):
         self.correct_color_button = QPushButton("True Color")
 
@@ -89,7 +97,7 @@ class Window(QWidget):
             f" {self.correct_color[2]})")
         self.correct_color_button.setFixedSize((400 - 50), (200 - 50))
 
-        self.layout.addWidget(self.correct_color_button, 0, FIELD_X_COUNT + 1)
+        self.layout.addWidget(self.correct_color_button, 0, self.active_difficulty + 1)
 
     # --- create tries button ---
     def create_tries_label(self):
@@ -97,7 +105,7 @@ class Window(QWidget):
 
         self.tries_label.setStyleSheet("font-size: 18px")
         self.tries_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.layout.addWidget(self.tries_label, 1, FIELD_X_COUNT + 1)
+        self.layout.addWidget(self.tries_label, 1, self.active_difficulty + 1)
 
     def update_tries_on_click(self, _type):
         if _type == "set":
@@ -120,7 +128,16 @@ class Window(QWidget):
             f" {self.correct_color[2]})")
 
         self.check_clicked_button()
-        
+
+    # --- Menu ---
+    # todo:
+    def navbar(self):
+        # button = QPushButton("Hello World")
+        # menu_bar = QToolBar(self)
+        # self.addToolBar(menu_bar)
+        # self.layout.addWidget(menu_bar, 0, 0)
+        print("#")
+
     # --------------------------------------------------------------------------------
 
 
@@ -128,7 +145,15 @@ def main():
     app = QApplication(sys.argv)
 
     window = Window()
-    window.show()
+    main_window = QMainWindow()
+    main_window.setCentralWidget(window)
+    toolbar = QToolBar("Hi")
+    button_action = QAction("Your button", main_window)
+    button_action.setStatusTip("This is your button")
+    button_action.triggered.connect(lambda: print(f"test"))
+    toolbar.addAction(button_action)
+    main_window.addToolBar(toolbar)
+    main_window.show()
     window.run()
 
     try:
