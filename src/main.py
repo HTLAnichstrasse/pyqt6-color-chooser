0fa1206c8f243fc7cbd1d32b2342e17cfb2d6ce5
lucastusch <lucas.tusch@gmail.com>
2023-02-07 23:06:32 +0100

    MainWindow class

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from PyQt6.QtGui import QIcon, QAction
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QApplication, QWidget, QGridLayout, QMessageBox, QLineEdit, QLabel, QPushButton, \
     QMainWindow, QFileDialog, QVBoxLayout, QMenu, QMenuBar, QToolBar, QStatusBar
+from PyQt6.uic.properties import QtGui
 
 # constants
 DIFFICULTIES = {
@@ -17,6 +18,10 @@ WINDOW_HEIGHT = 400
 WINDOW_WIDTH = 800
 
 
+# todo: implement icons to display save and load options nicely - currently the save & load option is just an empty
+#  gray icon
+# todo: write cards and navbar in a separate class object
+# todo: also write an class for every grid layout and then add it to the window_handler function
 # todo: Difficulties: Easy(2|2) Medium(3|3) Hard(4|4) Impossible(5|5)
 # todo: save game & load game
 
@@ -35,12 +40,15 @@ class Window(QWidget):
 
     def run(self):
         self.window_ui()
-        self.navbar()
+        # self.navbar()
         self.create_guess_buttons()
         self.get_correct_color()
         self.create_correct_button()
         self.create_tries_label()
         self.check_clicked_button()
+
+    def close_window(self):
+        Window.close(self)
 
     def window_ui(self):
         self.layout.setContentsMargins(20, 20, 20, 20)
@@ -105,9 +113,10 @@ class Window(QWidget):
 
         self.tries_label.setStyleSheet("font-size: 18px")
         self.tries_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.correct_color_button.setFixedSize((400 - 50), (200 - 50))
         self.layout.addWidget(self.tries_label, 1, self.active_difficulty + 1)
 
-    def update_tries_on_click(self, _type):
+    def update_tries_on_click(self, _type: str):
         if _type == "set":
             self.tries += 1
             self.tries_label.setText(f"Tries: {self.tries}")
@@ -129,37 +138,110 @@ class Window(QWidget):
 
         self.check_clicked_button()
 
-    # --- Menu ---
-    # todo:
-    def navbar(self):
-        # button = QPushButton("Hello World")
-        # menu_bar = QToolBar(self)
-        # self.addToolBar(menu_bar)
-        # self.layout.addWidget(menu_bar, 0, 0)
-        print("#")
-
+    # --------------------------------------------------------------------------------
     # --------------------------------------------------------------------------------
 
 
-def main():
+class MainWindow(QMainWindow):
+    def __init__(self, window):
+        super().__init__()
+        self.layout = QGridLayout()
+
+        self.menu = self.menuBar()
+
+        self.menu_file_button_save_file = QAction("Save File")
+        self.menu_file_button_load_file = QAction("Load File")
+
+        self.buttons = window.buttons
+        self.correct_color = window.correct_color
+
+    def run(self):
+        self.window_ui()
+        self.menu_file()
+
+    def close_main_window(self):
+        MainWindow.close(self)
+
+    def window_ui(self):
+        self.layout.setContentsMargins(20, 20, 20, 20)
+        self.setLayout(self.layout)
+        self.setWindowTitle("Color Chooser")
+        self.setFixedSize(WINDOW_WIDTH, WINDOW_HEIGHT)
+
+    # --------------------------------------------------------------------------------
+
+    # --- read & write func ---
+    def read_from_file(self, filename):
+        try:
+            with open(filename, "r") as file:
+                return file.readlines()
+
+        except FileNotFoundError:
+            self.close_main_window()
+
+    def write_in_file(self, filename, written_data):
+        try:
+            with open(filename, "w") as file:
+                file.write(written_data + "\n")
+
+        except FileNotFoundError:
+            self.close_main_window()
+
+    # --- file handling ---
+    def menu_file_load_file(self):
+        file_filter = ["Text files (*.txt)", "All files (*)"]
+        filters_filer = ";;".join(file_filter)
+
+        filename = QFileDialog.getOpenFileName(self, filter=filters_filer, initialFilter=filters_filer[0])
+
+        read_data = self.read_from_file(filename[0])[0]
+        print(read_data)
+
+    def menu_file_save_file(self):
+        file_filter = ["Text files (*.txt)", "All files (*)"]
+        filters_filer = ";;".join(file_filter)
+
+        filename = QFileDialog.getSaveFileName(self, filter=filters_filer, initialFilter=filters_filer[0])[0]
+
+        self.write_in_file(filename, str(list(self.buttons)))
+        print(str(list(self.buttons)))
+
+    def menu_file(self):
+        menu_file = self.menu.addMenu("&File")
+        menu_file.addSeparator()
+
+        menu_file.addAction(self.menu_file_button_save_file)
+        menu_file.addAction(self.menu_file_button_load_file)
+
+        self.menu_file_button_save_file.triggered.connect(lambda: self.menu_file_save_file())
+        self.menu_file_button_load_file.triggered.connect(lambda: self.menu_file_load_file())
+
+        # self.setStyleSheet("background-color:rgb(230, 230, 230)")
+
+    # --------------------------------------------------------------------------------
+    # --------------------------------------------------------------------------------
+
+
+def window_handler():
     app = QApplication(sys.argv)
 
     window = Window()
-    main_window = QMainWindow()
-    main_window.setCentralWidget(window)
-    toolbar = QToolBar("Hi")
-    button_action = QAction("Your button", main_window)
-    button_action.setStatusTip("This is your button")
-    button_action.triggered.connect(lambda: print(f"test"))
-    toolbar.addAction(button_action)
-    main_window.addToolBar(toolbar)
-    main_window.show()
+    # window.show()
     window.run()
+
+    main_window = MainWindow(window)
+    main_window.setCentralWidget(window)
+    main_window.show()
+    main_window.run()
 
     try:
         app.exec()
     except KeyboardInterrupt:
-        print("Quited Game, Interrupt")
+        print("KeyboardInterrupt")
+
+
+def main():
+    window_handler()
 
 
 if __name__ == '__main__':
